cea6ba18448eb4febda5895ad4c166e4ad85d5c5
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-11-05 20:39:57 +0600

    реализовал создание продукта и вывод списка продуктов

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from webapp.views import product_list_view, product_create_view
 
 urlpatterns = [
-
+    path('', product_list_view, name='main'),
+    path('products/', product_list_view, name='product_list'),
+    path('products/add/', product_create_view, name='product_add'),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from webapp.models import Category, Product
 
-# Create your views here.
+def product_list_view(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+def product_create_view(request):
+    categories = Category.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        image = request.POST.get('image_url')
+        category_id = request.POST.get('category_id')
+        product = Product.objects.create(title=title, description=description, price=price, image=image, category_id=category_id)
+        return redirect('product_list')
+
+    return render(request, 'article_create.html', {'categories': categories})
