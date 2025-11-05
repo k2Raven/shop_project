34d22ca66578c9ae33565b728bae3c0f3589bb83
Aleksei_Kuzmin <leha.21.05.1996@gmail.com>
2025-11-05 20:05:37 +0600

    Создал модели для продукта и категории и зарегистрировал в админ панели

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='Название')
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=50, verbose_name="Название")
+    description = models.TextField(null=True, blank=True, verbose_name='Описание')
+    category = models.ForeignKey('webapp.Category', on_delete=models.RESTRICT, related_name='products', verbose_name='Категория')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата и время добавления')
+    price = models.DecimalField(max_digits=7, decimal_places=2, verbose_name='Стоимость')
+    image = models.URLField(verbose_name='Изображение')
+
+    def __str__(self):
+        return f'{self.id} - {self.title}'
